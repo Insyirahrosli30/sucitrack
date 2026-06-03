4ecab1b45e6caf66ff02dc19f4cba02493ada363
Wan Nur Insyirah binti Wan Rosli <insyirahrosli30@gmail.com>
2026-06-03 22:24:17 +0800

    Add my initial feature code

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,18 @@
 <?php
-
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', fn () => view('welcome'));
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 1. PLACE THE CALENDAR ROUTE HERE (OUTSIDE AUTH MIDDLEWARE)
+Route::get('/calendar', fn () => view('calendar'))->name('calendar');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', fn () => view('dashboard'))->name('dashboard');
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::patch('/profile', [ProfileController::class, 'update']);
+    Route::delete('/profile', [ProfileController::class, 'destroy']);
 });
 
 require __DIR__.'/auth.php';
